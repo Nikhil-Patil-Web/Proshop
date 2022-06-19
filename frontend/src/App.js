3832c24df6b8d96d2f1ee10c358654f50256dbac
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-19 23:02:53 +0530

    We have added the template cards for displaying the products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header'
 import {Container}  from 'react-bootstrap'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
     <Header />
-    <main className='py-3'> <Container><h1>Welcome to Proshop</h1></Container></main>
+    <main className='py-3'> <Container><HomeScreen></HomeScreen></Container></main>
     <Footer />
     
     </>
