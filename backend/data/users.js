4d986ba9c222f18d0910ceb2d30d3607e4badc56
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-21 10:37:28 +0530

    I have added the model files for user, product and order.

---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: 'Nikhil Patil',
+        email: 'nikhil@example.com',
+        password: xxxx
+    },
+    {
+        name: 'Anita Patil',
+        email: 'anita@example.com',
+        password: xxxx
+    },
+
+]
