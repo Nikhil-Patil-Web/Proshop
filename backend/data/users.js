fc11964bc93f26eab19cce5a66769e628abcd0a6
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-21 10:44:14 +0530

    I have added the template user data for testing.

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,19 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true
     },
     {
         name: 'Nikhil Patil',
         email: 'nikhil@example.com',
-        password: xxxx
+        password: bcrypt.hashSync('123456', 10)
     },
     {
         name: 'Anita Patil',
         email: 'anita@example.com',
-        password: xxxx
+        password: bcrypt.hashSync('123456', 10)
     },
-
 ]
+
+export default users
