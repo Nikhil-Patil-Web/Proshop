feb4ef2e2220bd85888afb6c9ced92bba85a3187
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-22 07:47:47 +0530

    We have started with redux implementation

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,12 @@ import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
 const router = express.Router()
 
-
 // @desc Route to fetch all products
 // @route GET api/products
 // @acess Public
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find();
-    res.json(products)
+    res.json(products);
 }))
 
 // @desc Route to fetch a single product
@@ -20,7 +19,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if(product){
         res.json(product)
     }else{
-        res.status(404).json({'message':'Product was not found'});
+        res.status(404)
+        throw new Error('Product not found')
     }  
 }));
 
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,22 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js'
-
-
-
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 dotenv.config();
 connectDB()
 
 const app = express();
 
+app.use((req, res, next) =>{
+    console.log('HELLO')
+    console.log(req.originalUrl)
+    next();
+})
+
 app.use('/api/products', productRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send('API is running......')
