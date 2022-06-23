0565f7cb7b0eb749da6d3b5451a0fa67fe381cf8
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-23 15:55:35 +0530

    I have added the middleware function required to hash the password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Authorize the user and get the token
 // @route POST api/users/login
@@ -16,13 +17,69 @@ const authUser = asyncHandler(async(req, res)=>{
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null
+        token: generateToken(user._id),
     })
  }else{
     res.status(401)
     throw new Error('Invalid email or password')
  }
- 
 })
 
-export  {authUser}
+
+// @desc Register a new user
+//@route POST api/users
+//@access Public
+
+const registerUser = asyncHandler(async(req, res)=>{
+    const {name, email, password} =req.body
+   
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error('User with the mail already exists')
+    }
+    
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid User Data');
+    }
+
+   })
+
+
+// @desc Get User profile
+// @route GET api/users/profile
+// @acess Private
+
+const getUserprofile = asyncHandler(async(req, res)=>{
+   const user = await User.findById(req.user._id);
+   if(user){
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    })
+   }else{
+    res.status(404)
+    throw new Error ('User not found')
+   }
+})
+
+
+
+export  {authUser,  registerUser, getUserprofile}
