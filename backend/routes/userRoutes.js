74bfda647a52b26a4de29a9689015fbafa3f11c8
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-25 07:58:47 +0530

    I have added the backend required to create a route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express'
-import {authUser, registerUser, getUserprofile, updateUserprofile} from '../controllers/userController.js'
+import {authUser,
+        registerUser, 
+        getUserprofile, 
+        updateUserprofile
+    } from '../controllers/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/login').post(authUser)
-router.route('/profile').get(protect, getUserprofile).put(protect, updateUserprofile)
-router.route('/').post(registerUser)
+router.route('/login')
+      .post(authUser)
+router.route('/profile')
+      .get(protect, getUserprofile)
+      .put(protect, updateUserprofile)
+router.route('/')
+      .post(registerUser)
 
 export default router
