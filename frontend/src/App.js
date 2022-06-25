e5bb68e343658351ff5807e96903d31e1fe3336e
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-25 07:29:14 +0530

    I have created the screen which displays all the order details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path ='/register' component={RegisterScreen}></Route>
         <Route path ='/profile' component={ProfileScreen}></Route>
         <Route path ='/payment' component={PaymentScreen}></Route>
+        <Route path='/placeorder' component={PlaceOrderScreen}></Route>
 
 
       </Container>
