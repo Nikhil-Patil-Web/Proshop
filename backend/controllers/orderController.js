86c000f506eb1423a48ccb6d8df3feebd20f59d5
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-25 21:52:42 +0530

    I have added the order pay reducer and the order payaction

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,5 +54,32 @@ else{
 })
 
 
+// @desc Updates the isPaid option to Paid
+// @route GET api/orders/:id/pay
+// @acess Private
 
-export {addOrderItems, getOrderById};
+
+const updateOrderToPaid = asyncHandler(async(req, res)=>{
+    const order = await Order.findById(req.params.id)
+    if(order){
+        order.isPaid =true;
+        order.paidAt =Date.now();
+        order.paymentResult={
+            id:req.body.id,
+            status:req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address
+        }
+        const updatedOrder = await order.save();
+        res.json(updatedOrder);
+    }
+    
+    else{
+        res.status(404)
+        throw new Error('Item not found')
+    }
+    })
+    
+
+
+export {addOrderItems, getOrderById, updateOrderToPaid};
