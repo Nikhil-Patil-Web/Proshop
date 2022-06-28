a42be52390e430a80e7adec6a3694e11e3b60ee5
Nikhil <nikhilganeshpatil@gmail.com>
2022-06-28 07:38:49 +0530

    I have added the front end functionality for image upload

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
@@ -21,6 +22,7 @@ const ProductEditScreen = ({match,history}) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
 
 
@@ -50,6 +52,27 @@ const ProductEditScreen = ({match,history}) => {
         }
       }
     }, [productId, dispatch, successUpdate])
+
+    const uploadFileHandler =async (e) =>{
+      const file = e.target.files[0]
+      const formData = new FormData()
+      formData.append('image', file)
+      setUploading(true)
+      try{
+        const config={
+          headers:{
+            'Content-Type':'multipart/form-data'
+          },
+        }
+        const {data} =await axios.post('/api/upload', formData, config)
+        setImage(data)
+        setUploading(false)
+      }catch(error){
+        console.error(error.response.data)
+        setUploading(false)
+      }
+  
+    }
 
     const submitHandler =(e) =>{
         e.preventDefault()
@@ -110,6 +133,14 @@ const ProductEditScreen = ({match,history}) => {
               placeholder='Enter Image URL'
               value={image}onChange={(e)=> setImage(e.target.value)}>
             </Form.Control>
+            <Form.Control
+            type='file'
+            id ='image-file'
+            label='Choose File'
+            custom 
+            onChange={uploadFileHandler}>
+            </Form.Control>
+            {uploading&&<Loader></Loader>}
         </Form.Group>
 
 
